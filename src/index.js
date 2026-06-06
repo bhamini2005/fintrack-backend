d1072b93678342357ce790df126fded320a5b88d
bhamini2005 <bhamini062005@gmail.com>
2026-06-06 13:20:39 +0530

    Update CORS origin for production deployment

--- src/index.js
+++ src/index.js
@@ -27,7 +27,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://fintrack-iota-umber.vercel.app",
+      "fintrack-app-expense.vercel.app",
       ],
     credentials: true,
   })
